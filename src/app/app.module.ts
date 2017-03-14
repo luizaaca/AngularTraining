d3e80327e6691f2b799adf198a89abaf94bf531c
Luiz Alberto de Andrade <Luiz Alberto de Andrade>
2017-03-14 17:15:46 -0300

    Fim do tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,40 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
-import { AppComponent }  from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
+import {HeroSearchComponent} from './hero-search.component';
+
 
 @NgModule({
-  imports:      
-  [ 
-	BrowserModule ,
-	FormsModule
+  imports:
+  [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
   ],
-  declarations: 
-  [ 
+  declarations:
+  [
     AppComponent,
-    HeroDetailComponent 
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent,
+    HeroSearchComponent
   ],
-  bootstrap:    [ AppComponent ]
+  providers: [HeroService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
